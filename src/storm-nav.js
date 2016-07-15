c73725968025e56ea53cc32e1c7b0504930d6d1d
mjbp <mick.perkins@stormid.com>
2016-07-15 11:25:27 +0100

    Adds first level nav

--- src/storm-nav.js
+++ src/storm-nav.js
@@ -11,31 +11,52 @@
             RETURN: 13,
             TAB: 9
         },
-        instances = [],
 		triggerEvents = ['click', 'keydown', 'touchstart'],
         defaults = {
-            tabSelector: '.js-nav-tab',
-            titleSelector: '.js-nav-title',
+            triggerSelector: '.js-nav-trigger',
+            targetSelector: '.js-nav-target',
             closeBtnSelector: '.js-nav-close',
             currentClass: 'active',
 			active: null
         },
         StormNav = {
 			init: function(){
-				this.tabs = [].slice.call(this.DOMElement.querySelectorAll(this.settings.tabSelector));
-				this.titles = [].slice.call(this.DOMElement.querySelectorAll(this.settings.titleSelector));
-				this.triggers = this.titles.concat(this.tabs);
-				this.targets = this.tabs.map(function(el){
-					return document.getElementById(el.getAttribute('href').substr(1)) || console.error('No subnav elements not found');
-				});
-                this.closeBtn = this.DOMElement.querySelector(this.settings.closeBtnSelector);
-					
+                //construct an object model of the nav indexed by type, then by id??
+                /* 
+                targetsBySection: {
+                    'resources': [],
+                    'nature': [],
+                    'community': []
+                },
+                triggersBySection: {
+                    'resources': DOMNode,
+                    'nature': DOMNode,
+                    'community': DOMNode
+                }
+                */
+                this.triggers = [].slice.call(document.querySelectorAll(this.settings.triggerSelector));
+                this.targets = [].slice.call(document.querySelectorAll(this.settings.targetSelector));
+                this.nav = {};
+                this.targetsBySection = {};
+                this.triggersBySection = {};
                 this.current = this.settings.active;
+
+                this.targets.forEach(function(target){
+                    this.targetsBySection[target.getAttribute('id')] = target;
+                }.bind(this));
+
+                for(var k in this.targetsBySection) {
+                    if(this.targetsBySection.hasOwnProperty(k)) {
+                        this.triggersBySection[k] = this.triggers.filter(function(trigger){
+                            return ((trigger.getAttribute('href') && trigger.getAttribute('href').substr(1)) || trigger.parentNode.getAttribute('id')) === k;
+                        }.bind(this));
+                    }
+                }
+
+                this.closeBtn = document.querySelector(this.settings.closeBtnSelector);
 				this.initAria();
-				!!this.tabs.length && this.initTriggers(this.tabs);
-				!!this.titles.length && this.initTriggers(this.titles);
-				!!this.current && this.openTab(this.current);
-                !!this.closeBtn && this.initCloseBtn();
+                this.initTriggers();
+                this.initCloseBtn();
                 return this;
 			},
 			initAria: function() {
@@ -66,78 +87,74 @@
                 }.bind(this));
             },
             initTriggers: function(triggers) {
-                var handler = function(i){
-                    this.toggle(i);
-                };
+                var initNavGroup = function(t, s){
+                        triggerEvents.forEach(function(ev){
+                            t.addEventListener(ev, function(e){
+                                if(!!e.keyCode && e.keyCode === KEY_CODES.TAB) { return; }
+                                if(!!!e.keyCode || e.keyCode === KEY_CODES.RETURN){
+                                    e.preventDefault();
+                                    this.toggle(s);
+                                }
+                            }.bind(this), false);
+                        }.bind(this));
+                    }.bind(this);
 
-                triggers.forEach(function(el, i){
-                    triggerEvents.forEach(function(ev){
-                        el.addEventListener(ev, function(e){
-                            if(!!e.keyCode && e.keyCode === KEY_CODES.TAB) { return; }
-                            if(!!!e.keyCode || e.keyCode === KEY_CODES.RETURN){
-                                e.preventDefault();
-                                handler.call(this, i);
-                            }
-                        }.bind(this), false);
-                    }.bind(this));
-                    
-                }.bind(this));
-
-                return this;
+                for(var section in this.triggersBySection) {
+                    if(this.triggersBySection.hasOwnProperty(section)) {
+                        this.triggersBySection[section].forEach(function(trigger){
+                            initNavGroup(trigger, section);
+                        }.bind(this));
+                    }
+                }
             },
-            changeTab: function(type, i) {
+            toggle: function(section) {
+				if(this.current === null) { 
+					this.changeTab('open', section);
+                    this.current = section;
+					return this;
+				}
+				if(this.current === section) { 
+					this.changeTab('close', section);
+                    this.current = null;
+                    return this;
+                }
+				 this.changeTab('close', this.current)
+					.changeTab('open', section);
+
+                this.current = section;
+				return this;
+			},
+            changeTab: function(type, section) {
                 var methods = {
                     open: {
                         classlist: 'add',
                         tabIndex: {
-                            target: this.targets[i],
+                            target: this.targetsBySection[section],
                             value: '0'
                         }
                     },
                     close: {
                         classlist: 'remove',
                         tabIndex: {
-                            target: this.targets[this.current],
+                            target: this.targetsBySection[section],
                             value: '-1'
                         }
                     }
                 };
 
-                this.tabs[i].classList[methods[type].classlist](this.settings.currentClass);
-                this.titles[i].classList[methods[type].classlist](this.settings.currentClass);
-                this.targets[i].classList[methods[type].classlist](this.settings.currentClass);
-                STORM.UTILS.attributelist.toggle(this.targets[i], 'aria-hidden');
-                STORM.UTILS.attributelist.toggle(this.tabs[i], ['aria-selected', 'aria-expanded']);
-                STORM.UTILS.attributelist.toggle(this.titles[i], ['aria-selected', 'aria-expanded']);
+                this.triggersBySection[section].forEach(function(trigger){
+                    console.log(trigger);
+                    trigger.classList[methods[type].classlist](this.settings.currentClass);
+                    STORM.UTILS.attributelist.toggle(trigger, ['aria-selected', 'aria-expanded']);
+                }.bind(this));
+                this.targetsBySection[section].classList[methods[type].classlist](this.settings.currentClass);
+                STORM.UTILS.attributelist.toggle(this.targetsBySection[section], 'aria-hidden');
                 STORM.UTILS.attributelist.set(methods[type].tabIndex.target, {
                     'tabIndex': methods[type].tabIndex.value
                 });
-            },
-            openTab: function(i) {
-                this.changeTab('open', i);
-                this.current = i;
+
                 return this;
-            },
-            closeTab: function(i) {
-                this.changeTab('close', i);
-                return this;
-            },
-            toggle: function(i) {
-				if(this.current === i) { 
-                    this.closeTab(this.current)
-                    this.current = null;
-                    return this;
-                }
-				if(this.current === null) { 
-					this.openTab(i);
-					return this;
-				}
-				 this.closeTab(this.current)
-					.openTab(i);
-				return this;
-			},
-            open: function(url){},
-            close: function(url){}
+            }
 		};
     
     function init(sel, opts) {
